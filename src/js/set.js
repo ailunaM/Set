ff8047b1ccb9b063d6871ace6f20736f507e7f5e
ailunaM <ajlunaravsanbekova@gmail.com>
2025-01-18 00:44:07 +0600

    task1

--- src/js/set.js
+++ src/js/set.js
@@ -1,1 +1,33 @@
 
+class Team {
+  constructor() {
+    this.members = new Set();
+  }
+  add(character) {
+    if(this.members.has(character)){
+        throw new Error('такой персонаж уже есть')
+    }
+    this.members.add(character)
+  }
+  addAll(...character) {
+    return character.forEach(character => this.members.add(character))
+
+  }
+  toArray() {
+    return [...this.members]
+  }
+}
+
+const char1 = {
+ name: 'Boromir'
+}
+const team = new Team([
+    char1,
+    char1
+])
+
+
+team.add(char1)
+team.addAll(char1,char1)
+console.log( team.toArray());
+
